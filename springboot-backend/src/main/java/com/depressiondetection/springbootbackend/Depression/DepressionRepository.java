60203b3036778cd5e81c660d934be5eac365f7a8
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-03-11 19:59:15 -0500

    configured get and post endpoints, connected to postgresql through jpa

--- springboot-backend/src/main/java/com/depressiondetection/springbootbackend/Depression/DepressionRepository.java
+++ springboot-backend/src/main/java/com/depressiondetection/springbootbackend/Depression/DepressionRepository.java
@@ -6,8 +6,4 @@ import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface DepressionRepository extends JpaRepository<Depression, Long> {
-    @Query("SELECT d FROM Depression d WHERE d.id = ?1")
-    Depression findDepressionBy(String input);
-
-
 }
