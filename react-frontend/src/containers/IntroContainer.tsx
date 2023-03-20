4f79ed7385f30112f29a48bcc9b0030196c43e8a
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-03-20 15:52:02 -0400

    bug fixes

--- react-frontend/src/containers/IntroContainer.tsx
+++ react-frontend/src/containers/IntroContainer.tsx
@@ -19,7 +19,7 @@ function IntroContainer() {
       <img
         src="src/assets/wire.svg"
         alt="wire"
-        className="w-[400px] scale-110 md:scale-100 md:w-[500px] animate-pulse mx-auto dark:filter invert"
+        className="w-[400px] scale-110 md:scale-100 md:w-[500px] animate-pulse mx-auto"
       ></img>
       <div className="w-80 md:w-[450px]">
         <h3 className="text-justify">
