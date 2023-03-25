7e7ae7f5e10b5f10fc6c5cd278736964758987cd
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-03-24 20:36:27 -0400

    utilities path added

---
+++ react-frontend/src/utilities/headers.tsx
@@ -0,0 +1,9 @@
+const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Max-Age": "86400",
+};
+
+export { headers };
