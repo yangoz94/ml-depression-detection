a465bcdf93f6504e6acc97cc94f56f89770488e0
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-10-23 20:52:33 -0400

    remove redundant imports

--- react-frontend/src/containers/DefinitionsContainer.tsx
+++ react-frontend/src/containers/DefinitionsContainer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Information from "../components/Information";
 
 function DefinitionsContainer() {
--- react-frontend/src/custom-hooks/useInputSender.tsx
+++ react-frontend/src/custom-hooks/useInputSender.tsx
@@ -1,16 +1,16 @@
 import { useState, useContext, useRef } from "react";
 import { RefContext } from "../contexts/RefContext";
 import { FormEvent } from "react";
-import {headers} from "../utilities/headers";
+import { headers } from "../utilities/headers";
 
 function useInputSender() {
   const context = useContext(RefContext);
   const [output, setOutput] = useState("");
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   const sendInput = async () => {
     if (context.inputRef.current?.value.length < 20) {
-      alert('Please enter at least 20 characters.');
+      alert("Please enter at least 20 characters.");
       return;
     }
 
@@ -20,8 +20,8 @@ function useInputSender() {
       headers: headers,
       mode: "cors",
       body: JSON.stringify({
-        input: context.inputRef.current?.value
-      })
+        input: context.inputRef.current?.value,
+      }),
     });
 
     if (response.ok) {
@@ -29,18 +29,22 @@ function useInputSender() {
       const output = data.statement;
       setOutput(output); // update the output state with the retrieved value
     } else {
-      console.log('Error:', response.status);
-      alert('Sorry, we used all our limited resources. Try again in a few seconds...: ' + response.status)
+      console.log("Response is ", response);
+      console.log("Error:", response.status);
+      alert(
+        "Sorry, we used all our limited resources. Try again in a few seconds...: " +
+          response.status
+      );
     }
-    setIsLoading(false);  
-  }
+    setIsLoading(false);
+  };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     sendInput();
-  }
+  };
 
-  return {output, isLoading, setOutput, handleSubmit }
+  return { output, isLoading, setOutput, handleSubmit };
 }
 
 export default useInputSender;
